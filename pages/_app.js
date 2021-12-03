55dd2bf6370cd05a22ad3a2af1a9684f3bd50504
Simone <startae14@gmail.com>
2021-12-03 12:00:23 +0100

    Create a footeer in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2021 Simo</p>
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
