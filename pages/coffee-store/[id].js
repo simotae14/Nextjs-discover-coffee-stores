065672b1c58a0cd98b7bdf5598448b6b769d9e85
Simone <startae14@gmail.com>
2021-12-04 00:55:47 +0100

    Create a Dynammic Page using the nextjs Link component

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -3,12 +3,17 @@ import Link from 'next/link'
 
 const CoffeeStore = () => {
   const router = useRouter()
-  console.log("router", router);
   return (
     <div>
       Coffee Store Page {router.query.id}
       <Link href="/">
         <a>Back to home</a>
+      </Link>
+      <Link href={{
+        pathname: '/coffee-store/[id]',
+        query: { id: 'my-post' },
+      }}>
+        <a>Go to page dynamic</a>
       </Link>
     </div>
   )
