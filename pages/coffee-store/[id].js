9aad2a5ebd837863986fc6621f159c8abdd854d5
Simone <startae14@gmail.com>
2021-12-04 00:36:01 +0100

    Dynamic routes  using useRouter from next/router

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,5 +1,9 @@
+import { useRouter } from 'next/router'
+
 const CoffeeStore = () => {
-  return (<div>Coffee Store Page</div>)
+  const router = useRouter()
+  console.log("router", router);
+  return (<div>Coffee Store Page {router.query.id}</div>)
 }
 
 export default CoffeeStore;
