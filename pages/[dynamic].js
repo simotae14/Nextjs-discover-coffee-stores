a99072dbb400f8e28a08b98441d6072f4daea8c6
Simone <startae14@gmail.com>
2021-12-04 01:13:07 +0100

    Add the Dynamiic value inside the dynamic page using useRouter and the Head component to modify the title

--- pages/[dynamic].js
+++ pages/[dynamic].js
@@ -1,10 +1,14 @@
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 const DynamicRoute = () => {
   const router = useRouter()
   const { dynamic } = router.query
   return (
     <div>
+      <Head>
+        <title>{dynamic}</title>
+      </Head>
       Hi there I am a dynamic route {dynamic}
     </div>
   )
