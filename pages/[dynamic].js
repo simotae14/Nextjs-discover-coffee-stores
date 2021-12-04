f223f1b4eca27e58417b2403d998afaddba50e2b
Simone <startae14@gmail.com>
2021-12-04 01:09:12 +0100

    Read the dynamic content sent to the Dynamic Route

--- pages/[dynamic].js
+++ pages/[dynamic].js
@@ -1,7 +1,11 @@
+import { useRouter } from 'next/router'
+
 const DynamicRoute = () => {
+  const router = useRouter()
+  const { dynamic } = router.query
   return (
     <div>
-      Hi there I am a dynamic route
+      Hi there I am a dynamic route {dynamic}
     </div>
   )
 }
