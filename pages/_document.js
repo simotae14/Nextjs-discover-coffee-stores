85fa60be75366718eed030875ad06469ec8ef6c5
Simone <startae14@gmail.com>
2021-12-06 11:38:35 +0100

    Add the Font immports inside the _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,26 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link
+            as="font"
+            rel="preload"
+            href="/fonts/IBMPlexSans-Bold.ttf"
+            crossOrigin="anonymous"
+          ></link>
+          <link
+            as="font"
+            rel="preload"
+            href="/fonts/IBMPlexSans-Regular.ttf"
+            crossOrigin="anonymous"
+          ></link>
+          <link
+            as="font"
+            rel="preload"
+            href="/fonts/IBMPlexSans-SemiBold.ttf"
+            crossOrigin="anonymous"
+          ></link>
+        </Head>
         <body>
           <Main></Main>
           <NextScript />
