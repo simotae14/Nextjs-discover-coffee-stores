37c022607786b47379580997e42a1923ccd8b410
Simone <startae14@gmail.com>
2021-12-06 17:54:03 +0100

    Add Card style  and Glassmorphism style to the Card component using also the classnames library

--- components/card.js
+++ components/card.js
@@ -1,19 +1,27 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import cls from 'classnames'
+
+import styles from './Card.module.css'
 
 const Card = (props) => {
   return (
     <Link href={props.href}>
-      <a>
-        <div>
-          <h2>{props.name}</h2>
-          <Image
-            alt="Card Image"
-            src={props.imgUrl}
-            width={260}
-            height={160}
-          />
-        </div>;
+      <a className={styles.cardLink}>
+        <div className={cls("glass", styles.container)}>
+          <div className={styles.cardHeaderWrapper}>
+            <h2 className={styles.cardHeader}>{props.name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image
+              className={styles.cardImage}
+              alt="Card Image"
+              src={props.imgUrl}
+              width={260}
+              height={160}
+            />
+          </div>
+        </div>
       </a>
     </Link>
   )
